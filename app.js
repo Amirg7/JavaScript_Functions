8c4d424a49f736be14c3e92462529dcd973eb19f
Amir Gaffar <AmirG7@yahoo.com>
2021-01-31 21:07:06 -0500

    Completed Exercise1 and Exercise2

--- app.js
+++ app.js
@@ -3,5 +3,31 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+    for(i = 1; i <= count; i++) 
+ if(i % 2 == 1) {
+    console.log(i);
+} else {
+    console.log("not odd");
+}
+
+};
+
+printOdds(88)
+
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(age) {
+    let aboveSixteen = "You can drive!";
+    let belowSixteen = "Sorry, but you need to wait until you're 16.";
+    if (age < 16){
+        console.log(belowSixteen);
+    } else { 
+        console.log(aboveSixteen);
+    }
+
+};
+
+checkAge(30);
